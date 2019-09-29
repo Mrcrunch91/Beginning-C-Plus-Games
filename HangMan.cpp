75e9cf689274671789ff594111f958a6a48c0a82
NateDogg <Alterac91@yahoo.com>
2019-09-29 11:01:05 -0500

    Fresh Start

---
+++ HangMan.cpp
@@ -0,0 +1,25 @@
+/*
+ * HangMan.cpp
+ *
+ *  Created on: Sep 29, 2019
+ *      Author: natedogg
+ */
+
+#include "HangMan.h"
+#include <iostream>
+
+using namespace std;
+
+HangMan::HangMan() {
+	// TODO Auto-generated constructor stub
+
+}
+
+HangMan::~HangMan() {
+	// TODO Auto-generated destructor stub
+}
+
+int main(){
+	cout<<"It Works Nate!"<<endl;
+}
+
---
+++ HangMan.h
@@ -0,0 +1,18 @@
+/*
+ * HangMan.h
+ *
+ *  Created on: Sep 29, 2019
+ *      Author: natedogg
+ */
+
+#ifndef HANGMAN_H_
+#define HANGMAN_H_
+
+class HangMan {
+
+public:
+	HangMan();
+	virtual ~HangMan();
+};
+
+#endif /* HANGMAN_H_ */
